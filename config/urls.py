80dff8cd0d21d2578b5fd9b5f51a76d212d47f10
gabrielPistore <devgbpistore@gmail.com>
2024-09-03 13:04:35 -0300

    add django_browser_reload

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("__reload__/", include("django_browser_reload.urls")),
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
 ]
--- run_server.py
+++ run_server.py
@@ -3,10 +3,12 @@ import threading
 
 
 def run_server():
+    print("Starting Django development server...\n")
     subprocess.run("python manage.py runserver", shell=True)
 
 
 def run_tailwind():
+    print("Starting Tailwind CSS watcher...\n")
     subprocess.run(
         "npx tailwindcss -i ./static/css/input.css -o ./static/css/output.css --watch",
         shell=True,
