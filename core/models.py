d5585cae7780c867d5b20a65efcddcdeff1d733c
gabrielPistore <devgbpistore@gmail.com>
2024-09-07 16:13:40 -0300

    modify work order fields

--- core/models.py
+++ core/models.py
@@ -36,21 +36,66 @@ class WorkOrder(models.Model):
         ("Alto", "Alto"),
         ("Crítico", "Crítico"),
     )
-
     STATUS = (
         ("Aberto", "Aberto"),
         ("Em Andamento", "Em Andamento"),
         ("Fechado", "Fechado"),
     )
+    DEPT_NAMES = (
+        ("", "Selecione"),
+        ("REITORIA", "REITORIA"),
+        ("COAD-GR", "COAD-GR"),
+        ("CCS", "CCS"),
+        ("NAI", "NAI"),
+        ("CIDARQ", "CIDARQ"),
+        ("BIBLIOTECA", "BIBLIOTECA"),
+        ("CDPEC", "CDPEC"),
+        ("EDITORA", "EDITORA"),
+        ("SETI", "SETI"),
+        ("SEAF", "SEAF"),
+        ("PREF", "PREF"),
+        ("CENTRAL DE OPERAÇÕES VIGILÂNCIA", "CENTRAL DE OPERAÇÕES VIGILÂNCIA"),
+        ("SERVIÇO DE LIMPEZA", "SERVIÇO DE LIMPEZA"),
+        ("DAA", "DAA"),
+        ("PROAF", "PROAF"),
+        ("DCMP", "DCMP"),
+        ("CPATRI", "CPATRI"),
+        ("DCF", "DCF"),
+        ("DLT", "DLT"),
+        ("PROEC", "PROEC"),
+        ("PROPESQ", "PROPESQ"),
+        ("CEP", "CEP"),
+        ("PROGEP", "PROGEP"),
+        ("SIASS", "SIASS"),
+        ("PRPE", "PRPE"),
+        ("PROGRAD", "PROGRAD"),
+        ("CGEN", "CGEN"),
+        ("FAE", "FAE"),
+        ("FENG - Engenharia de Minas", "FENG - Engenharia de Minas"),
+        ("FENG - Engenharia de Produção", "FENG - Engenharia de Produção"),
+        ("FENG - Secretaria Administrativa", "FENG - Secretaria Administrativa"),
+        ("FENG - Engenharia Civil", "FENG - Engenharia Civil"),
+        ("IBIOTEC - Ciências Biológicas", "IBIOTEC - Ciências Biológicas"),
+        ("IBIOTEC - Ciências da Computação", "IBIOTEC - Ciências da Computação"),
+        ("IBIOTEC - Educação Física", "IBIOTEC - Educação Física"),
+        ("IBIOTEC - Enfermagem", "IBIOTEC - Enfermagem"),
+        ("IBIOTEC - LABIM", "IBIOTEC - LABIM"),
+        ("IBIOTEC - Psicologia", "IBIOTEC - Psicologia"),
+        ("IBIOTEC - Medicina", "IBIOTEC - Medicina"),
+        ("IF", "IF"),
+        ("IGEO", "IGEO"),
+        ("INHCS", "INHCS"),
+        ("IMTec", "IMTec"),
+        ("IEL", "IEL"),
+        ("IQ", "IQ"),
+    )
 
     requested_by = models.CharField(max_length=255, verbose_name="Solicitante")
-    # add choices for department
-    dept_name = models.CharField(max_length=255, verbose_name="Departamento")
-    email = models.EmailField()
-    mobile_phone = models.CharField(max_length=20, verbose_name="Celular")
-    landline_phone = models.CharField(
-        max_length=20, verbose_name="Telefone Fixo", blank=True, null=True
+    dept_name = models.CharField(
+        max_length=32, choices=DEPT_NAMES, verbose_name="Departamento"
     )
+    email = models.EmailField()
+    phone = models.CharField(max_length=20, verbose_name="Celular")
     order_type = models.CharField(max_length=255, verbose_name="Tipo")
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, verbose_name="Categoria"
@@ -61,6 +106,7 @@ class WorkOrder(models.Model):
     impact = models.CharField(max_length=7, choices=LEVEL, verbose_name="Impacto")
     urgency = models.CharField(max_length=7, choices=LEVEL, verbose_name="Urgência")
     priority = models.CharField(max_length=7, choices=LEVEL, verbose_name="Prioridade")
+    location = models.CharField(max_length=255, verbose_name="Local do Serviço")
     opening_date = models.DateTimeField(verbose_name="Data de Abertura")
     closing_date = models.DateTimeField(verbose_name="Data de Fechamento")
     service_start_date = models.DateTimeField(
