5dfe07fddcc80e814eda5b0dece3a7d02c468149
gabrielpistore <devgbpistore@gmail.com>
2024-12-17 12:50:41 -0300

    💻 run tailwindcss along with development server using bin/dev

--- script/download_tailwindcss_cli.py
+++ script/download_tailwindcss_cli.py
@@ -3,26 +3,29 @@ import platform
 import stat
 from pathlib import Path
 
+import colorama
 import requests
 
+# Initialize colorama
+colorama.init(autoreset=True)
+
+# Base URL and paths
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Get OS and Architecture
 SYSTEM = platform.system().lower()
 MACHINE = platform.machine().lower()
 
-# Base URL
+# Base URL for TailwindCSS binary
 BASE_URL = f"https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-{SYSTEM}"
 
+# Determine the appropriate download URL based on the architecture
 if MACHINE in ["amd64", "x86_64"]:
     tailwindcss_url = f"{BASE_URL}-x64"
 elif "arm" in MACHINE or "aarch" in MACHINE:
     tailwindcss_url = f"{BASE_URL}-arm64"
 else:
-    raise Exception(f"Unsupported architecture: {MACHINE}")
-
-if SYSTEM == "windows":
-    tailwindcss_url += ".exe"
+    raise Exception(f"{colorama.Fore.RED}Unsupported architecture: {MACHINE}{colorama.Style.RESET_ALL}")
 
 # Output directory
 output_dir = BASE_DIR / "bin"
@@ -34,22 +37,19 @@ if not os.path.exists(output_dir):
 # Set the filename based on the OS
 filename = "tailwindcss"
 
-if SYSTEM == "windows":
-    filename = "tailwindcss.exe"
-
 output_file = BASE_DIR / output_dir / filename
 
 # Download the TailwindCSS CLI standalone binary
-print(f"Downloading TailwindCSS CLI from {tailwindcss_url}...")
+print(f"{colorama.Fore.CYAN}Downloading TailwindCSS CLI from {tailwindcss_url}...")
 response = requests.get(tailwindcss_url)
 
 if response.status_code == 200:
     with open(output_file, "wb") as file:
         file.write(response.content)
-    print(f"Downloaded TailwindCSS CLI to {output_file}")
+    print(f"{colorama.Fore.GREEN}Downloaded TailwindCSS CLI to {output_file}{colorama.Style.RESET_ALL}")
 else:
     raise Exception(
-        f"Failed to download TailwindCSS CLI. Status code: {response.status_code}"
+        f"{colorama.Fore.RED}Failed to download TailwindCSS CLI. Status code: {response.status_code}{colorama.Style.RESET_ALL}"
     )
 
 # Make the downloaded file executable (not needed on Windows)
@@ -57,4 +57,4 @@ if SYSTEM != "windows":
     st = os.stat(output_file)
     os.chmod(output_file, st.st_mode | stat.S_IEXEC)
 
-print("TailwindCSS CLI is ready to use!")
+print(f"{colorama.Fore.GREEN}TailwindCSS CLI is ready to use!{colorama.Style.RESET_ALL}")
