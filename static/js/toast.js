bcbae8397360747e5e7b458a4dc5f9b38ea00a31
gabrielpistore <devgbpistore@gmail.com>
2025-02-05 07:45:09 -0300

    💄 improve messages

--- static/js/toast.js
+++ static/js/toast.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
-    document.querySelectorAll(".flash-message").forEach(function (message) {
+    document.querySelectorAll(".message").forEach(function (message) {
       message.style.transition = "opacity 0.5s";
       message.style.opacity = "0";
       setTimeout(() => message.remove(), 500);
